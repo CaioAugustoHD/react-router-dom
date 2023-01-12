71666e9bcab812951d23aa05e942a74c0eac99d3
CaioAugustoHD <caioaugustosbs@gmail.com>
2023-01-11 21:36:26 -0300

    Criação de rotas na pasta pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { About } from "./pages/About"
+import { Contact } from "./pages/Contact"
+import { Home } from "./pages/Home"
 
 export function App() {
   return (
-    <h1>Hello!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
