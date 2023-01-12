19ea55ab3dec4a7296a2b9fb7f53008bb2e371d0
CaioAugustoHD <caioaugustosbs@gmail.com>
2023-01-11 22:05:09 -0300

    Criado componente Header com Menu de Links das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Header } from "./components/Header"
 import { About } from "./pages/About"
 import { Contact } from "./pages/Contact"
 import { Home } from "./pages/Home"
@@ -6,6 +7,7 @@ import { Home } from "./pages/Home"
 export function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
